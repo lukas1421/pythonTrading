f2da8d1e08bef93a4a660479456ffa0e164760b7
lukas1421 <shijiluke@gmail.com>
2019-02-12 14:50:25 +0800

    2.12 convert to float, round ts raw data

--- venv/live.py
+++ venv/live.py
@@ -5,7 +5,12 @@ def getIndexLive():
     df = ts.get_realtime_quotes(['sh000001', 'sh000016', 'sh000300', 'sz399001', 'sz399006', 'sh000905'])
     df['Ret'] = ((df['price'].astype('float') / df['pre_close'].astype('float') - 1) * 100) \
         .apply(lambda x: round(x, 2))
+    df[['open', 'pre_close', 'price', 'high', 'low']] \
+        = df[['open', 'pre_close', 'price', 'high', 'low']].astype(float) \
+        .round(2)
+
     print(df.iloc[:, [0, 2, 3, 30, 31, 32, 33]])
+    # print(df.iloc[:, [0, 2, 3, 30, 31, 32, 33]])
 
 
 getIndexLive()
