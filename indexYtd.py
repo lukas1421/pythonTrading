5cd6ae29afbdc16447165579ad7838fc29c10b91
lukas1421 <shijiluke@gmail.com>
2019-02-13 18:41:20 +0800

    2 13

--- indexYtd.py
+++ indexYtd.py
@@ -3,32 +3,40 @@ import datetime as dt
 
 indices = ['sh000001', 'sh000016', 'sh000300', 'sz399006', 'sh000905', 'sz399001']
 
-
 # indices = ['sh000001']
+startDate = dt.datetime.now() + dt.timedelta(days=-3)
+startDateStr = startDate.strftime('%Y-%m-%d')
+endDate = dt.datetime.now() + dt.timedelta(days=+1)
+endDateStr = endDate.strftime('%Y-%m-%d')
+dateFormat = '%Y-%m-%d'
 
 
 def get5mData():
     for index in indices:
         print('***********************', index)
-        df = ts.get_hist_data(index, ktype='5', start='2019-02-12')
-        df1 = df[['open', 'high', 'low', 'close']].sort_values(['date'])
-        df1['time'] = df1.index
-        df1['time'] = df1['time'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-        pmOpen = df1['open'][df1['time'].dt.time > dt.time(12, 0, 0)].values[0]
-        pmClose = df1['close'].tail(1).values[0]
-        dayHigh = df1['high'].max()
-        dayLow = df1['low'].min()
+        df = ts.get_hist_data(index, ktype='5', start=startDateStr, end=endDateStr)
+        df['time'] = df.index
+        df['time'] = df['time'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        df1 = df[['time', 'open', 'high', 'low', 'close']].sort_values(['date'])
         date = df1['time'].dt.date.tail(1).values[0]
+        df2 = df1[df1['time'].dt.date == date]
+
+        pmOpen = df2['open'][df2['time'].dt.time > dt.time(12, 0, 0)].values[0]
+        pmClose = df2['close'].tail(1).values[0]
+        dayHigh = df2['high'].max()
+        dayLow = df2['low'].min()
 
         print('index', index)
-        print(list([date, 'pmChg%', str(round((pmClose / pmOpen - 1) * 10000.0) / 100.0) + '%', 'closingP',
-                    str(round(10000.0 * (pmClose - dayLow) / (dayHigh - dayLow)) / 100.0) + '%']));
+        print(list(
+            [date.strftime(dateFormat), 'pmChg%', str(round((pmClose / pmOpen - 1) * 10000.0) / 100.0) + '%',
+             'closingP', str(round(10000.0 * (pmClose - dayLow) / (dayHigh - dayLow)) / 100.0) + '%']))
 
-    def getDayData():
-        for index in indices:
-            df = ts.get_hist_data(index, ktype='D', start='2019-02-12')
-            df1 = df[['open', 'high', 'low', 'close']].sort_values(['date'])
-            print('index', df1)
+
+def getDayData():
+    for index in indices:
+        df = ts.get_hist_data(index, ktype='D', start=startDateStr)
+        df1 = df[['open', 'high', 'low', 'close']].sort_values(['date'])
+        print('index', df1)
 
 
 get5mData();
